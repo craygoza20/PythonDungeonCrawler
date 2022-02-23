5e1a420ca49dd193af2e1de1a1d94344ef7c88ca
Carlos <74159984+craygoza20@users.noreply.github.com>
2022-02-23 11:43:49 -0800

    Add project file to repo

---
+++ FinalProject.py
@@ -0,0 +1,97 @@
+"""
+Created by: Carlos Raygoza
+Date: 2/22/2022
+Description: A small text adventure game where you will be able to create your own
+             character and go on a few quests!
+
+Sources: Codecademy Python3 course, W3Schools, StackOverflow
+
+"""
+
+# TODO: Create class for characters (Player, Enemies, NPCs)
+# TODO: Create system for locations
+# TODO: Create quest lines
+# TODO: Create a combat loop function
+# TODO: Create a shopping loop function
+
+
+# There will be three main groups
+# Player, Enemy, NPC
+class Character:
+    
+    inventory = {}
+    health = 0
+    coins = 0
+    attackPower = 0
+    defensePower = 0
+
+    def __init__(self, name, level, group, coins=0, health=8, attackPower=5, defensePower=2):
+        self.name = name
+        self.level = level
+        self.group = group
+        if self.group == 'Player':
+            self.health = 15
+            self.attackPower = 6
+            self.defensePower = 2
+            self.coins = 20
+        else:
+            self.health = health
+            self.attackPower = attackPower
+            self.defensePower = defensePower
+            self.coins = coins
+
+    def __repr__(self):
+        if self.group == 'Player':
+            return f"""
+            Your character is named {self.name}. {self.name} is level {self.level}. 
+            {self.name} has {self.coins} coins.
+            {self.name}'s attack power is {self.attackPower}. {self.name}'s defense power is {self.defensePower}."""
+        elif self.group == 'Enemy': # TODO: add repr for Enemies
+            pass
+        elif self.group == 'NPC': # TODO: add repr for NPC (shopkeeps)
+            pass    
+    # TODO: need actions for characters (attack, use items, buy items, talk to others, etc..)
+    
+    def attack(self, enemy):
+        attackpoints = self.attackPower * (1 / enemy.defensePower)
+        enemy.health -= attackpoints
+        print(f"{self.name} has attacked {enemy.name} for {attackpoints} health.")
+        if enemy.health <= 0:
+            print(f"{enemy.name} has been slain by {self.name}.")
+        else:
+            print(f"{enemy.name} has {enemy.health} health points remaining.")
+        pass
+
+    def useItem(self, item):
+        pass
+
+    def buy(self, item, shopkeep):
+        pass
+
+    def speak(self, NPC):
+        pass
+
+c_name = input("Please type in your character's name: ")
+player = Character(c_name, 1, 'Player')
+print(player)
+
+print("""
+The warmth of the sun radiates across your body.
+Birds are chirping, the leaves are rustling from the breeze.
+The cool morning air penetrates your nostrils.
+Dirt, moss, a hint of wintergreen.
+\"Where am I?\"
+You hear a faint snort coming from behind.
+""")
+
+choice1 = int(input("""
+Choose a number:
+1. Go towards the noise.
+2. Get away from the noise.
+"""))
+
+if choice1 == 1:
+    print("""
+    You go towards the source of the noise and find yourself face to face with a wild boar!
+    The boar begins to squeal and is preparing to charge at you!
+    """)
